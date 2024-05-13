137201f117407e13bacd7cc23618cb16641c57a2
este <31979112+wlalswjd@users.noreply.github.com>
2024-05-13 22:32:44 +0200

    Added reset button

--- blur-blind-mode.user.js
+++ blur-blind-mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Popsauce blur blind mode
 // @namespace    https://github.com/wlalswjd
-// @version      0.0.3
+// @version      0.0.4
 // @description  popsauce blur blind mode
 // @author       este
 // @match        *.jklm.fun/games/popsauce/
@@ -13,11 +13,12 @@
 
     const blurBlindArea = document.createElement('div');
     blurBlindArea.innerHTML = `
-<div class="blur-mode" style="display: flex; justify-content: center; gap: 8px;">
+<div class="blur-mode" style="display: flex; justify-content: center; align-items: center; gap: 8px;">
 <label for="blind"><input id="blind" type="checkbox" style="margin-right: 8px; transform: scale(1.5);">Blind mode, </label>
 <span>Blur mode: </span>
 <input id="blur-range" type="range" value="0" min="0" max="50" step="10">
 <span style="width:55px;" class="blurValue">0px</span>
+<button type="button" id="reset" style="border-radius: 50%; padding: 0 6px; font-size: 20px;">↻</button>
 </div>
 `
 
@@ -29,6 +30,7 @@
 
     const blind = document.querySelector("#blind");
     const blurRange = document.querySelector("#blur-range");
+    const reset = document.querySelector("#reset");
 
     blurRange.addEventListener("change", () => {
         const value = blurRange.value;
@@ -45,8 +47,16 @@
             image.setAttribute("style", "opacity: 0;");
             text.setAttribute("style", "opacity: 0;");
         } else {
-            image.setAttribute("style", "");
-            text.setAttribute("style", "");
+            image.removeAttribute("style");
+            text.removeAttribute("style");
         }
+    });
+
+    reset.addEventListener("click", () => {
+        blind.checked = false;
+        blurRange.value = 0;
+        blurValue.textContent = "0px";
+        image.removeAttribute("style");
+        text.removeAttribute("style");
     });
 })();
